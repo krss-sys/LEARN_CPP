b729e2465c7df92aaa80f8b1a67392828e6c1f1e
krss-sys <kgm.naoya@gmail.com>
2026-04-17 08:25:52 +0700

    lession 1 c++

---
+++ research/chapter_01/01_lession/01_homework/01_homework.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"Chao cac ban"<< endl;
+    cout <<"Minh la newbie"<< endl;
+    cout <<"Minh se thanh thao sau 1 nam"<< endl;
+    return 0;
+}
---
+++ research/chapter_01/01_lession/01_learn/01_console_output.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout <<"i'm newbie"<< endl;
+    return 0;
+}
