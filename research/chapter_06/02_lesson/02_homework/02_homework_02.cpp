ba677ec5aa07f3891c5897e75df539957a1ea05f
krss-sys <kgm.naoya@gmail.com>
2026-08-25 07:50:05 +0700

    Update 02_homework_02.cpp

--- research/chapter_06/02_lesson/02_homework/02_homework_02.cpp
+++ research/chapter_06/02_lesson/02_homework/02_homework_02.cpp
@@ -1,21 +1,17 @@
+#include <chrono>
 #include <iostream>
 #include <mutex>
-#include <chrono>
 #include <thread>
 
 class TaiKhoan {
-private:
+   private:
     int soDu = 1000;
     std::mutex mtx;
 
-public:
-    TaiKhoan() {
-        std::cout << "Constructor!\n";
-    }
+   public:
+    TaiKhoan() { std::cout << "Constructor!\n"; }
 
-    ~TaiKhoan() {
-        std::cout << "Destructor!\n";
-    }
+    ~TaiKhoan() { std::cout << "Destructor!\n"; }
 
     void unlockSom(std::string ten) {
         std::unique_lock<std::mutex> lock(mtx);
@@ -25,7 +21,7 @@ public:
         }
         lock.unlock();
         std::cout << "Da unlock som\n";
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     void deferLock(std::string ten) {
@@ -44,11 +40,11 @@ public:
             std::this_thread::sleep_for(std::chrono::seconds(1));
             if (soDu >= 300) {
                 soDu -= 300;
-                std::cout << "Da rut 300, so du: " << soDu << "\n";            
+                std::cout << "Da rut 300, so du: " << soDu << "\n";
             } else {
                 std::cout << "So du khong du\n";
             }
-            
+
         } else {
             std::cout << ten << " dang co rut 300...\n";
             std::cout << "Tai khoan dang bi khoa, thu lai sau\n";
